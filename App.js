27506b28043af7a4ba944874aed0a1f26098bbc3
drew4u <drewsibert@gmail.com>
2018-04-04 15:59:17 -0400

    Updating my shit

--- App.js
+++ App.js
@@ -1,9 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
  import React, { Component } from 'react';
  import { Provider } from 'mobx-react';
  import AppNavigator from './app/app.navigator';
